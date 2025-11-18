1f76a4590eb9bbe96dc3cdfa6c8711c4e23e92d3
Mikhan-TMP <mikhanbalbastro@outlook.com>
2025-11-18 09:56:33 +0800

    IDK what i added. It was a long time ago

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:4001', 
+    origin: 'http://localhost:4001 ', 
     credentials: true,
   });
 
